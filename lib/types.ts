619db8c84f521f3c4063c158b21692f7de911dbf
Tiago Flora <tiagomflora@gmail.com>
2023-06-24 09:57:58 -0700

    define shapes object

--- lib/types.ts
+++ lib/types.ts
@@ -11,3 +11,41 @@ export enum Block { // This will be changed when we generate multiminos with dif
 }
 
 export type BoardShape = Block[][]; // Matrix of cells, either filled or empty
+
+export const SHAPES = {
+    I : [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+    ],
+    J : [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0],
+    ],
+    T : [
+        [0, 0, 0],
+        [0, 1, 0],
+        [1, 1, 1],
+    ],
+    S : [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0],
+    ],
+    L : [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1],
+    ],
+    O : [
+        [1, 1],
+        [1, 1],
+    ],
+    Z : [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1],
+    ],
+}
